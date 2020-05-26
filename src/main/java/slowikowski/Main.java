bb3d8a1d423a63ed9066a900af4bd56c81410d43
Kuba <sekenuw@gmail.com>
2020-05-26 17:23:19 +0200

    First commit

---
+++ src/main/java/slowikowski/Main.java
@@ -0,0 +1,61 @@
+package slowikowski;
+
+
+import java.util.Scanner;
+
+class Main {
+
+    private String firstSequence;
+    private String secondSequence;
+
+    private void readData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszy ciag: ");
+        firstSequence = scanner.nextLine();
+        System.out.println("Podaj drugi ciag: ");
+        secondSequence = scanner.nextLine();
+    }
+
+    boolean isSubsequence(String sequence, String subSequence) {
+        int seqLength = sequence.length();
+        int subLength = subSequence.length();
+        int j = 0;
+
+        for (int i = 0; i < seqLength && j < subLength; i++) {
+
+            if (subSequence.charAt(j) == '*') {
+                for (int jj = j + 1; jj < subLength && i < seqLength; i++) {
+                    if(subSequence.charAt(jj) == sequence.charAt(i)) {
+                        j++;
+                        jj++;
+                    }
+                    break;
+                }
+            }
+
+            if(subSequence.charAt(j) == '\\' && j+1 < subLength && subSequence.charAt(j+1)== '*') {
+                subLength--;
+                j++;
+            }
+
+            if (subSequence.charAt(j) == sequence.charAt(i)) {
+                j++;
+            }
+        }
+
+        return (j == subLength);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.readData();
+
+        System.out.println(main.isSubsequence(main.firstSequence, main.secondSequence));
+    }
+}
+
+
+
+
+
+
---
+++ src/test/java/slowikowski/MainTest.java
@@ -0,0 +1,86 @@
+package slowikowski;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    private static Main main = new Main();
+
+    @Test
+    void str2_should_be_subsequence_of_str1() {
+        String str1 = "123456789";
+        String str2 = "167";
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void str2_should_be_subsequence_of_str1_2() {
+        String str1 = "123456789";
+        String str2 = "";
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void str2_should_be_subsequence_of_str1_3() {
+        String str1 = "";
+        String str2 = "";
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    @DisplayName(value = "Test should pass with given empty strings")
+    void str2_should_be_subsequence_of_str1_4() {
+        String str1 = "";
+        String str2 = "";
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void str2_withSlash_should_be_subsequence_of_str1() {
+        String str1 = "123\\";
+        String str2 = "123\\";
+        System.out.println(str2.length());
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void str2_should_not_be_subsequence_of_str1() {
+        String str1 = "123456789";
+        String str2 = "341";
+        assertFalse(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void str2_withStar_should_be_subsequence_of_str1() {
+        String str1 = "abc";
+        String str2 = "a*c";
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void str2_withSlashAndStar_should_be_subsequence_of_str1() {
+        String str1 = "a*ccd";
+        String str2 = "a\\*cc";
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void should_Throw_NullPointerException() {
+        String str1 = "a*ccd";
+        String str2 = null;
+        assertThrows(NullPointerException.class, () -> main.isSubsequence(str1, str2));
+    }
+
+    @Test
+    void str2_withSlashAndStar_should_be_subsequence_of_str1_2() {
+        String str1 = "*";
+        String str2 = "\\*";
+        System.out.println(str2.length());
+        assertTrue(main.isSubsequence(str1, str2));
+    }
+
+
+}
